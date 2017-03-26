cd8dd3a353eb3c4a6ae75f2ed5f7fd352935d2df
YJ <->
2017-03-26 09:23:01 +0800

    add changes

---
+++ display.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+
+   $(function () {
+    "use strict";
+
+    // for better performance - to avoid searching in DOM
+    //var content = $('#content');
+    //var input = $('#input');
+    //var status = $('#status');
+
+    // my color assigned by the server
+    //var myColor = false;
+    // my name sent to the server
+    //var myName = false;
+
+    // if user is running mozilla then use it's built-in WebSocket
+    window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+    // if browser doesn't support WebSocket, just show some notification and exit
+    if (!window.WebSocket) {
+        content.html($('<p>', { text: 'Sorry, but your browser doesn\'t '
+                                    + 'support WebSockets.'} ));
+        input.hide();
+        $('span').hide();
+        return;
+    }
+
+    // open connection
+    var connection = new WebSocket("ws://127.0.0.1:8080/");
+
+    connection.onopen = function () {
+		alert("Connection open");
+        // first we want users to enter their names
+        //input.removeAttr('disabled');
+        //status.text('Choose name:');
+    };
+
+    connection.onerror = function (error) {
+        // just in there were some problems with conenction...
+        content.html($('<p>', { text: 'Sorry, but there\'s some problem with your '
+                                    + 'connection or the server is down.' } ));
+    };
+
+    // most important part - incoming messages
+    connection.onmessage = function (message) {
+        // try to parse JSON message. Because we know that the server always returns
+        // JSON this should work without any problem but we should make sure that
+        // the massage is not chunked or otherwise damaged.
+        try {
+            var json = JSON.parse(message.data);
+        } catch (e) {
+            console.log('This doesn\'t look like a valid JSON: ', message.data);
+            return;
+        }
+
+		
+		alert("receive message");
+    };
+	
+	connection.onclose = function() {
+    alert("Closed!");
+	};
+
+    /**
+     * This method is optional. If the server wasn't able to respond to the
+     * in 3 seconds then show some error message to notify the user that
+     * something is wrong.
+     */
+    setInterval(function() {
+        if (connection.readyState !== 1) {
+            status.text('Error');
+            input.attr('disabled', 'disabled').val('Unable to comminucate '
+                                                 + 'with the WebSocket server.');
+        }
+    }, 3000);
+});
+});
